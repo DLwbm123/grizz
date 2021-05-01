2c922a855016d17b75048ad128933eac9a163292
brysonwx <brysonwx@163.com>
2021-05-02 01:18:32 +0800

    add some comments for knn

--- knn.py
+++ knn.py
@@ -13,8 +13,9 @@ class Knn(object):
 
     def knn_v1(self):
         X_train, X_test, y_train, y_test = self._get_train_and_test_data()
-        # KNeighborsClassifier belongs to sklearn package
-        # n_neighbors is K
+        # n_neighbors: 就是K
+        # metric: 样本之间距离度量公式，默认为闵可夫斯基距离
+        # p: 默认值为2，即为欧几里德距离
         clf = KNeighborsClassifier(n_neighbors=3)
         clf.fit(X_train, y_train)
         # To caculate the accuracy score
@@ -45,21 +46,28 @@ class Knn(object):
         :param k: the number of neighbors
         :return:
         '''
+        # 计算已知类别数据集中的点与当前点之间的距离
         distances = [self.get_euclidean_distance(x, testInstance) for x in X]
+        # 按照距离次序排序并选取与当前点距离最小的k个点
         kneighbors = np.argsort(distances)[:k]
+        # 确定前k个点所在类别的出现频率
         count = Counter(y[kneighbors])
+        # 返回前k个点出现频率最高的类别作为当前点的预测分类
         return count.most_common()[0][0]
 
     def _get_train_and_test_data(self):
-        # load iris dataset
+        # 加载iris数据集
         iris = datasets.load_iris()
         x = iris.data
         y = iris.target
-        # split data into training data & test data
-        X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=2003)
+        # 拆分数据集为训练集和测试集
+        # random_state代表随机种子编号，保证程序每次运行都分割一样的训练集 & 测试集
+        # shuffle代表是否进行有放回抽样
+        # test_size表示切割多少百分比的数据做为测试集
+        X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=2003, shuffle=True)
         return (X_train, X_test, y_train, y_test)
 
 if __name__ == '__main__':
     knn_obj = Knn()
-    knn_obj.knn_v1()
+    # knn_obj.knn_v1()
     knn_obj.knn_v2()
